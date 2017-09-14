2ffa7458e4bf0fd8ab466e30888d646fe6699a18
TurkatsoAV <Turkatso.Alexander@gmail.com>
2017-09-14 17:31:20 +0400

    configure static resources

--- src/main/java/com/tur/jusus/config/WebAppConfig.java
+++ src/main/java/com/tur/jusus/config/WebAppConfig.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
@@ -14,7 +16,12 @@ import org.springframework.web.servlet.view.JstlView;
 @EnableWebMvc
 @ComponentScan({"com.tur.jusus.controller","com.tur.jusus.service"})
 @Import({SecurityConfig.class})
-public class WebAppConfig {
+public class WebAppConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/css/**").addResourceLocations("/WEB-INF/css/");
+    }
 
     @Bean
     public InternalResourceViewResolver setupViewResolver() {
--- src/main/webapp/WEB-INF/pages/index.jsp
+++ src/main/webapp/WEB-INF/pages/index.jsp
@@ -6,7 +6,10 @@
 <html>
 <head>
     <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Title</title>
+    <link href="<c:url value="/css/bootstrap.css" />" rel="stylesheet">
 </head>
 <body>
 <div class="container">
