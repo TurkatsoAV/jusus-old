02c99bcfb30fcd4ea67cc71402a47c7fcc53a489
TurkatsoAV <Turkatso.Alexander@gmail.com>
2017-09-14 13:53:19 +0400

    add Bootstrap v3.3.2 and remove unusing imports

--- src/main/java/com/tur/jusus/config/WebAppConfig.java
+++ src/main/java/com/tur/jusus/config/WebAppConfig.java
@@ -6,10 +6,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
