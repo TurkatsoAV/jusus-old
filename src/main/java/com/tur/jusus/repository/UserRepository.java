e27a9860bf47edd62eb60e04ba6186005b35255d
TurkatsoAV <Turkatso.Alexander@gmail.com>
2018-02-21 17:42:09 +0400

    revert extends to JpaRepository

--- src/main/java/com/tur/jusus/repository/UserRepository.java
+++ src/main/java/com/tur/jusus/repository/UserRepository.java
@@ -4,6 +4,6 @@ import com.tur.jusus.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
     User findByLogin(String userName);
 }
